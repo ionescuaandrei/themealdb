7eb65ce2794bc28582899333774c4f25f94cd9ab
Andrei Ionescu <andreiionescu750@gmail.com>
2024-01-20 13:25:10 +0200

    .

--- atestatinfo/src/components/Navbar.js
+++ atestatinfo/src/components/Navbar.js
@@ -2,8 +2,13 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className="navbar">
-      gigi
+    <div className="navbar container">
+      <a href="#!" className="logo">F<span>oo</span>diesInfo</a>
+      <div className="nav-links">
+        <a href="#!">Home</a>
+        <a href="#!">Recipes</a>
+        <a href="#!">Settings</a>
+      </div>
     </div>
   )
 }
